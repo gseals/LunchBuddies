6ede67759e9d503ee50f3892b9e94e2cb31f5a66
Gabriel Seals <sealsgj@gmail.com>
2020-03-14 13:53:25 -0500

    Add project files.

---
+++ LunchBuddies/LunchBuddy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchBuddies
+{
+    class LunchBuddy
+    {
+        public LunchBuddy()
+        {
+            var firstName = "";
+            var lastName = "";
+        }
+    }
+}
---
+++ LunchBuddies/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchBuddies
+{
+    class Restaurant
+    {
+        private List<string> RestaurantList { get; set; }
+
+        public Restaurant()
+        {
+
+        }
+
+    }
+
+}
